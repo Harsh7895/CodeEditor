52de911d23026c49ff7be87a10666f6eb69e3e10
Harsh7895 <harshsik7890@gmail.com>
2026-01-23 23:42:12 +0530

    background jobs through ingest

---
+++ src/app/api/demo/blocking/route.ts
@@ -0,0 +1,12 @@
+// Post localhost:300/api/demo/blocking
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+
+export async function POST() {
+  const response = await generateText({
+    model: google("gemini-2.5-flash"),
+    prompt: "Write a vegetaring lasagna recipe for 4 people.",
+  });
+
+  return Response.json({ response });
+}
