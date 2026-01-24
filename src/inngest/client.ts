b2212c76b6d12c0c3136e8a5ac5173d9271049e3
Harsh7895 <harshsik7890@gmail.com>
2026-01-25 02:44:20 +0530

    error tracking through sentry

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,3 +1,7 @@
 import { Inngest } from "inngest";
+import { sentryMiddleware } from "@inngest/middleware-sentry";
 
-export const inngest = new Inngest({ id: "code_editor" });
+export const inngest = new Inngest({
+  id: "code_editor",
+  middleware: [sentryMiddleware()],
+});
